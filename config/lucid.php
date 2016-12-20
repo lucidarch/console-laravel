79b17e8ac647844fab076467d220400a41756367
speccode <github@speccode.com>
2016-12-20 07:18:05 +0000

    misspells

--- config/lucid.php
+++ config/lucid.php
@@ -6,9 +6,9 @@ return [
      | Dashboard
      |--------------------------------------------------------------------------
      |
-     | By default /lucid/dashboard is avilable on when env('APP_DEBUG') is true.
-     | If you set this value to "true" it will be always accessable even on
-     | production environment.
+     | By default /lucid/dashboard is available on when env('APP_DEBUG')
+     | is true. If you set this value to "true" it will be always accessible
+     | even on production environment.
      |
      */
     'dashboard'     => null,
@@ -23,9 +23,10 @@ return [
      | etc. will be generated into /app/ directory.
      |
      | When you set this value to "false" it means you application have multiple
-     | services and all classes will be generated into /src/ directrory. After
-     | changing that value all "lucid" classes should be moved from /app/ to
-     | /src/.
+     | services and all classes will be generated into /src/ directrory.
+     | After changing that value all "lucid" classes should be moved
+     | from /app/ to /src/.
+     |
      */
      //'microservice'  => true,
 
